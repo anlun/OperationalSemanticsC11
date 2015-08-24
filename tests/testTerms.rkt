66f1e9d3a6984977675ecc9f673dc2a490b69faf
Podkopaev Anton <podkoav239@gmail.com>
2015-08-24 16:05:44 +0300

    Moved some test terms from monadsLang.

--- tests/testTerms.rkt
+++ tests/testTerms.rkt
@@ -80,6 +80,118 @@ With postponed reads it shouldn't be able to lead to R1 = R2 = 1, because of rel
 
 
 #|
+y_rel  = 1 || x_rel = 1
+R1 = x_acq || R2 = y_acq
+
+Can lead to R1 = R2 = 0.
+|#
+(define testTerm1
+        (term ((write rel "x" 0) >>= (λ x
+              ((write rel "y" 0) >>= (λ x
+              ((spw
+                   ((write rel "y" 1) >>= (λ x
+                   ((read  acq "x")   >>= (λ x
+                    (ret x)))))
+                   ((write rel "x" 1) >>= (λ x
+                   ((read  acq "y")   >>= (λ x
+                    (ret x))))))
+                  >>=
+                  (λ x (ret x)))))))))
+
+
+#|
+x_na = 1 || x_na = 2
+
+It should get `stuck`.
+|#
+(define testTerm2
+        (term ((spw
+                   ((write na "x" 1) >>= (λ x (ret 1)))
+                   ((write na "x" 2) >>= (λ x (ret 2))))
+                  >>=
+                  (λ x (ret x)))))
+
+#|
+       c_rel = 0;
+a_na  = 7; || repeat (c_acq) end;
+c_rel = 1  || a_na = a_na + 1
+       ret a_na
+
+Example from: VafeiadisNarayan:OOPSLA13 "Relaxed Separation Logic: A Program Logic for C11 Concurrency".
+It shouldn't get `stuck`.
+|#
+(define testTerm3
+         (term (((write rel "c" 0) >>= (λ x
+                    (spw
+                     ((write na  "a" 7) >>= (λ x
+                      (write rel "c" 1)))
+                     ((repeat (read acq "c")) >>= (λ x
+                     ((read  na "a") >>= (λ x
+                      (write na "a" (+ 1 x))))
+                      ))
+                    )))
+                    >>= (λ x (read na "a")))))
+
+
+#|
+Dekker's lock doesn't work in weak memory settings (and in our model).
+
+               x_rlx = 0;
+               y_rlx = 0;
+x_rel = 1;         || y_rel = 1;
+if y_acq == 0 then || if x_acq == 0 then
+  a_na = 239            a_na = 30
+
+It should get `stuck` because of concurrent non-atomic writes.
+|#
+(define testTerm4
+            (term ((write rel "x" 0) >>= (λ x
+                  ((write rel "y" 0) >>= (λ x
+                   (spw
+                    ((write rel "x" 1) >>= (λ x
+                    ((read  acq "y"  ) >>= (λ y
+                     (if (== 0 y) (write na "a" 239) (ret 0))))))
+                    ((write rel "y" 1) >>= (λ x
+                    ((read  acq "x"  ) >>= (λ x
+                     (if (== 0 x) (write na "a" 30 ) (ret 0))))))
+                    )
+                    )) ))))
+
+
+#|
+Ernie Cohen's lock should work in weak memory settings.
+Described in Turon-al:OOPSLA14.
+
+                   x_rel = 0;
+                   y_rel = 0;
+x_rel = choice(1, 2);  || y_rel = choice(1, 2); 
+repeat y_acq end;      || repeat x_acq end;
+if x_acq == y_acq then || if x_acq != y_acq then
+  a_na = 239           ||   a_na = 239
+
+
+Unfortunately, DrRacket can't find fixpoint in normal time in this case.
+|#
+(define testTerm5
+          (term ((write rlx "x" 0) >>= (λ x
+                ((write rlx "y" 0) >>= (λ x
+                ((spw
+                   ((write rel "x" (choice 1 2))  >>= (λ x
+                   ((repeatFuel 1 (read acq "y")) >>= (λ x
+                   ((read acq "x") >>= (λ x
+                   ((read acq "y") >>= (λ y
+                    (if (== x y) (write na "a" 239) (ret 0))))))))))
+
+                   ((write rel "y" (choice 1 2))  >>= (λ x
+                   ((repeatFuel 1 (read acq "x")) >>= (λ x
+                   ((read acq "x") >>= (λ x
+                   ((read acq "y") >>= (λ y
+                    (if (!= x y) (write na "a" 239) (ret 0))))))))))
+                   
+                  ) >>= (λ x ((read na "a") >>= (λ x (ret x)))))))))))
+
+
+#|
                      x_rlx = 0
 x_rlx = 1 || x_rlx = 2 || a = x_rlx || c = x_rlx
           ||           || b = x_rlx || d = x_rlx
@@ -148,3 +260,38 @@ In TSO a = 1 and b = 0 is forbidden outcome. But not in our semantics.
              (ret (a b))))))
             ) >>= (λ x
             (ret (proj2 x))))))))))
+
+#|
+cas rlx sc "x" 1 0
+
+Leads to `stuck` state, because Compare-and-Set (Read-Modify-Write) operations in C11 are
+undefined if success modifier is weaker than fail modifier.
+|#
+(define testTerm8
+  (term (cas rlx sc "x" 1 0)))
+
+#|
+An example from VafeiadisNarayan:OOPSLA13. It shouldn't get `stuck`.
+
+                    lock = 1
+a_na     = 2 || if (cas_acq_rlx lock 0 1) then || if (cas_acq_rlx lock 0 1)
+lock_rel = 0 ||    a_na = 3                    ||    a_na = 2
+                    ret a
+|#
+
+(define testTerm9
+     (term ((write rlx "lock" 1) >>= (λ x
+            (spw
+             ((write na "a" 2) >>= (λ x
+              (write rel "lock" 0)))
+             (spw
+              ((cas acq rlx "lock" 0 1) >>= (λ x
+               (if x
+                   (write na "a" 3)
+                   (ret -1))))
+              ((cas acq rlx "lock" 0 1) >>= (λ x
+               (if x
+                   (write na "a" 2)
+                   (ret -1))))
+              ))))))
+
