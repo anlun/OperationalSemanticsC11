c8fb6f01cd24beeac86387586562aa5750d133bf
Podkopaev Anton <podkoav239@gmail.com>
2016-02-01 13:40:30 +0300

    Added an CAS example.

--- test/relAcqNaRlxPost_t1.rkt
+++ test/relAcqNaRlxPost_t1.rkt
@@ -7,10 +7,45 @@
 (require "../core/parser.rkt")
 
 #|
+             x_rlx = 0; a_rlx = 0
+a_rlx = 1 || cas_rel_rlx(x, 1, 2) || rX = x_acq
+x_rel = 1 ||                      || rA = a_rlx
+             ret (rX rA)
+
+Possible outcomes according to Batty-al:POPL11:
+rX |rA  
+--------
+0  |0,1
+1  |1  
+2  |1  
+|#
+(define term_WW_WRMW_RR
+  @prog{x_rlx := 0;
+        a_rlx := 0;
+        r0 := spw
+              {{{ spw
+                  {{{ a_rlx := 1;
+                      x_rel := 1
+                  ||| cas_rel_rlx(x, 1, 2) }}}
+              ||| rX := x_acq;
+                  rA := a_rlx;
+                  ret [rX rA] }}};
+        ret r0_2 })
+
+(test-->> step (term (,term_WW_WRMW_RR defaultState))
+
+          (term ((ret (0 0)) defaultState))
+          (term ((ret (0 1)) defaultState))
+
+          (term ((ret (1 1)) defaultState))
+          
+          (term ((ret (2 1)) defaultState)))
+
+#|
                      x_rlx = 0; a_rlx = 0; b_rlx = 0
-a_rlx = 1 || b_rlx = 1            || if (x_rlx = 2) { || rX = x_acq
-x_rel = 1 || cas_rel_rlx(x, 1, 2) ||   x_rlx = 3      || rA = a_rlx
-                                  || } else { ret 0 } || rB = b_rlx
+a_rlx = 1 || b_rlx = 1            || if (x_rlx == 2) { || rX = x_acq
+x_rel = 1 || cas_rel_rlx(x, 1, 2) ||   x_rlx = 3       || rA = a_rlx
+                                  || } else { ret 0 }  || rB = b_rlx
                           ret (rX (rA rB))
 
 Possible outcomes according to Batty-al:POPL11:
@@ -19,7 +54,7 @@ rX |rA  |rB
 0  |0,1 |0,1
 1  |1   |0,1
 2  |1   |1
-3  |0,1 |1
+3  |0,1 |0,1
 |#
 
 (define term_WW_WRMW_W_RRR
@@ -44,6 +79,7 @@ rX |rA  |rB
                       ret [rX [rA rB]] }}} }}};
         ret r0_2_2 })
 
+#|
 (test-->> step (term (,term_WW_WRMW_W_RRR defaultState))
 
           (term ((ret (0 (0 0))) defaultState))
@@ -56,8 +92,11 @@ rX |rA  |rB
           
           (term ((ret (2 (1 1))) defaultState))
 
+          (term ((ret (3 (0 0))) defaultState))
           (term ((ret (3 (0 1))) defaultState))
+          (term ((ret (3 (1 0))) defaultState))
           (term ((ret (3 (1 1))) defaultState)))
+|#
 
 ;(traces step (term (,term_WW_WRMW_W_RRR defaultState)) #:pp pretty-printer)
 
