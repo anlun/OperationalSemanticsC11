ce93c7c55d8ef07c111c3c7799f01654ad32841b
Podkopaev Anton <podkoav239@gmail.com>
2016-02-03 09:39:06 +0300

    Added CAS term for graph construction testing.

--- test/relAcqNaGraphTests.rkt
+++ test/relAcqNaGraphTests.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang at-exp racket
 (require redex/reduction-semantics)
 (require "../core/syntax.rkt")
 (require "../core/coreLang.rkt")
@@ -9,6 +9,7 @@
 (require "../core/pp.rkt")
 (require "../core/graphUtils.rkt")
 (require "../core/langs.rkt")
+(require "../core/parser.rkt")
 
 (define-term initialGraph  (Graph  (((0 skip)) ())))
 (define-term initialGFront (GFront 0))
@@ -47,6 +48,36 @@ It should get `stuck` because of concurrent non-atomic writes.
          (term (,testTerm4 defaultState))
          (term (stuck defaultState)))
 
-;(traces step (term (,testTerm4 defaultState)) #:pp pretty-printer)
-;(stepper step (term (,testTerm4 defaultState)) pretty-printer)
-;(stepper step (term (,testTerm3-3 defaultState)) pretty-printer)
+;; (traces step (term (,testTerm4 defaultState)) #:pp pretty-printer)
+;; (stepper step (term (,testTerm4 defaultState)) pretty-printer)
+;; (stepper step (term (,testTerm3-3 defaultState)) pretty-printer)
+
+#|
+               x_rel = 0;
+               y_rel = 0;
+cas_rel_acq(x, 0, 1) || cas_rel_acq(x, 0, 1)
+if y_acq == 0 then   || if x_acq == 0 then
+  a_na = 239            a_na = 30
+
+It should get `stuck` because of concurrent non-atomic writes.
+|#
+(define term_CASif_CASif
+  @prog{x_rel := 0;
+        y_rel := 0;
+        spw
+        {{{ x_rel := 1;
+            r0 := y_acq;
+            if r0 == 0
+            then a_na := 239
+            else ret 0
+            fi
+        ||| y_rel := 1;
+            r1 := x_acq;
+            if r1 == 0
+            then a_na := 30
+            else ret 0
+            fi }}} })
+
+(test-->>∃ step
+         (term (,term_CASif_CASif defaultState))
+         (term (stuck defaultState)))
